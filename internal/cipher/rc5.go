fa7f4c063f6019e6a3a83a60c9b9c4d25ddee63f
hahah-eto-ya-maga <alexbaivik1998@gmail.com>
2025-12-09 16:16:54 +0400

    Шифрование, дешифрование, правильно или нет даже не знаю

--- internal/cipher/rc5.go
+++ internal/cipher/rc5.go
@@ -1,8 +1,10 @@
 package cipher
 
 import (
+	"encoding/binary"
 	"fmt"
 	"kiberbez/internal/ui/colors"
+	"strconv"
 	"strings"
 )
 
@@ -72,11 +74,87 @@ func (c *RC5) Encrypt(text string) []string {
 		j = (j + 1) % len(L)
 	}
 
+	data := []byte(text)
+
+	if len(data)%8 != 0 {
+		pad := 8 - (len(data) % 8)
+		data = append(data, make([]byte, pad)...)
+	}
+
+	var ciphertext string
+	for b := 0; b < len(data); b += 8 {
+		A := binary.LittleEndian.Uint32(data[b : b+4])
+		B := binary.LittleEndian.Uint32(data[b+4 : b+8])
+
+		A += S[0]
+		B += S[1]
+
+		for round := 1; round <= r; round++ {
+			A = rotl32(A^B, B) + S[2*round]
+			B = rotl32(B^A, A) + S[2*round+1]
+		}
+
+		block := fmt.Sprintf("%08x%08x", A, B)
+		ciphertext += block
+	}
+	result = append(result, ciphertext)
+
 	return result
 }
 
 func (c *RC5) Decrypt(text string) []string {
 	var result []string
+
+	key := c.Key.Key
+	r := c.Key.Rounds
+	t := 2 * (r + 1)
+
+	S := make([]uint32, t)
+	S[0] = P
+	for i := 1; i < t; i++ {
+		S[i] = S[i-1] + Q
+	}
+
+	L := makeWordArray(key)
+
+	G, H := uint32(0), uint32(0)
+	i, j := 0, 0
+	n := max(3*len(S), 3*len(L))
+	for k := 0; k < n; k++ {
+		G = rotl32(S[i]+G+H, 3)
+		S[i] = G
+		H = rotl32(L[j]+G+H, G+H)
+		L[j] = H
+		i = (i + 1) % len(S)
+		j = (j + 1) % len(L)
+	}
+
+	var decoded []byte
+	for b := 0; b < len(text); b += 16 {
+		blockHex := text[b : b+16]
+
+		A, _ := strconv.ParseUint(blockHex[:8], 16, 32)
+		B, _ := strconv.ParseUint(blockHex[8:], 16, 32)
+
+		A32 := uint32(A)
+		B32 := uint32(B)
+
+		for round := r; round >= 1; round-- {
+			B32 = rotr32(B32-S[2*round+1], A32) ^ A32
+			A32 = rotr32(A32-S[2*round], B32) ^ B32
+		}
+
+		B32 -= S[1]
+		A32 -= S[0]
+
+		buf := make([]byte, 8)
+		binary.LittleEndian.PutUint32(buf[:4], A32)
+		binary.LittleEndian.PutUint32(buf[4:], B32)
+
+		decoded = append(decoded, buf...)
+	}
+
+	result = append(result, string(decoded))
 
 	return result
 }
@@ -90,6 +168,10 @@ func (c *RC5) Hack(text string) []string {
 
 func rotl32(x, y uint32) uint32 {
 	return (x << (y & 31)) | (x >> (32 - (y & 31)))
+}
+
+func rotr32(x, y uint32) uint32 {
+	return (x >> (y & 31)) | (x << (32 - (y & 31)))
 }
 
 func makeWordArray(key []byte) []uint32 {
